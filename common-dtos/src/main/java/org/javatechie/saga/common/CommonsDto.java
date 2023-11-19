f2104ae8f0855428ec9563250191293747586b56
Jagdish <1jagdish.tawde@gmail.com>
2023-11-19 12:49:18 +0530

    Initial multi module project for saga design pattern

---
+++ common-dtos/src/main/java/org/javatechie/saga/common/CommonsDto.java
@@ -0,0 +1,7 @@
+package org.javatechie.saga.common;
+
+public class CommonsDto {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ order-service/src/main/java/org/javatechie/saga/order/OrderService.java
@@ -0,0 +1,13 @@
+package org.javatechie.saga.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderService {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(OrderService.class);
+  }
+}
